84a7ecf6ced736a82362e388bfefe07d4aacc61a
Daniel Phiri <danielphiri@Daniels-MacBook-Pro.local>
2022-01-26 10:17:10 -0800

    Finished implementing UICollectionViewLayout in MainViewController.

--- Dispo Take Home/Main/MainViewController.swift
+++ Dispo Take Home/Main/MainViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
 
 class MainViewController: UIViewController {
+  
+  private let screenBounds : CGRect = UIScreen.main.bounds
+  private let padding      : CGFloat = .zero
+  private let cellsHeight  : CGFloat  = 134
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,7 +31,14 @@ class MainViewController: UIViewController {
 
   private var layout: UICollectionViewLayout {
     // TODO: implement
-    fatalError()
+    let layout                     = UICollectionViewFlowLayout()
+    layout.itemSize                = CGSize(
+                                          width  : screenBounds.width - (padding * 2),
+                                          height : cellsHeight
+                                        )
+    layout.scrollDirection         = .vertical
+    layout.minimumInteritemSpacing = padding
+    return layout
   }
 
   private lazy var collectionView: UICollectionView = {
