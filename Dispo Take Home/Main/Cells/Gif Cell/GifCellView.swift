842270eea2cde0e1381641e73135d3078e8b470e
Daniel Phiri <danielphiri@Daniels-MacBook-Pro.local>
2022-01-26 16:19:54 -0800

    Finished implementing network fetch via Clientable. Added Extensions file for UIColor to add a dark color that adapts to user theme.

---
+++ Dispo Take Home/Main/Cells/Gif Cell/GifCellView.swift
@@ -0,0 +1,12 @@
+//
+//  GifCellView.swift
+//  Dispo Take Home
+//
+//  Created by Daniel Phiri on 1/26/22.
+//
+
+import UIKit
+
+class GifCellView: UICollectionViewCell {
+    
+}
---
+++ Dispo Take Home/Util/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Dispo Take Home
+//
+//  Created by Daniel Phiri on 1/26/22.
+//
+
+import Foundation
